a56909055bd7c17c4c5ce9082e1ffbf0c3fa846f
Anuj Pophali <anujpophali@gmail.com>
2023-07-11 00:28:30 +0530

    first commit

---
+++ client/src/components/DefaultLayout.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Menu, Dropdown } from "antd";
+import { useNavigate } from "react-router-dom";
+
+import "../resources/default-layout.css";
+function DefaultLayout(props) {
+  const user = JSON.parse(localStorage.getItem("expenso"));
+  const navigate = useNavigate();
+  const menu = (
+    <Menu
+      items={[
+        {
+          label: (
+            <li
+              onClick={() => {
+                localStorage.removeItem("expenso");
+                navigate("/login");
+              }}
+            >
+              Logout
+            </li>
+          ),
+        },
+      ]}
+    />
+  );
+  return (
+    <div className="layout">
+      <div className="header d-flex justify-content-between align-items-center">
+        <div>
+          <h1 className="logo">Expenso</h1>
+        </div>
+        <div>
+          <Dropdown overlay={menu} placement="bottomLeft">
+            <button className="primary">{user.name}</button>
+          </Dropdown>
+        </div>
+      </div>
+
+      <div className="content">{props.children}</div>
+    </div>
+  );
+}
+
+export default DefaultLayout;
